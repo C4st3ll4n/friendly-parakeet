27dc89e0c77baf7f0fcf1d8b3aa63bf6ec6b02ac
Pedro Henrique <p13dr0h@gmail.com>
2023-05-01 18:50:43 -0300

    feat: add use case implementation

---
+++ mottu_marvel/lib/data/usecase/remote_load_characters.dart
@@ -0,0 +1,27 @@
+import 'package:mottu_marvel/data/infra/http/http.dart';
+import 'package:mottu_marvel/data/model/remote_character_model.dart';
+import 'package:mottu_marvel/domain/entity/character_entity.dart';
+import 'package:mottu_marvel/domain/usecase/load_characters.dart';
+
+class RemoteLoadCharacters implements LoadCharacters{
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteLoadCharacters(this.httpClient, this.url);
+
+  @override
+  Future<List<CharacterEntity>> load() async {
+    try{
+      final response = await httpClient.request(url: url, method: "get");
+      return response
+          .map<CharacterEntity>((json) => RemoteCharacterModel.fromJson(json).toEntity())
+          .toList();
+
+    }catch(e){
+      throw Exception("Deu ruim");
+    }
+
+    return [];
+  }
+
+}
